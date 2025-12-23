4534ee827c9deb04ddeeebd34bef5eee3672f3fd
Nich <nicholausdei2@gmail.com>
2025-12-23 22:25:20 +0700

    firebase configurations

---
+++ capstone_pms/functions/src/index.ts
@@ -0,0 +1,4 @@
+import {setGlobalOptions} from "firebase-functions";
+import {onRequest} from "firebase-functions/https";
+import * as logger from "firebase-functions/logger";
+setGlobalOptions({ maxInstances: 10 });
