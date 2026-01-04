8cce93d71d8e93a894b364c788e3d0e4bdecac4b
Nich <nicholausdei2@gmail.com>
2026-01-05 02:13:53 +0700

    allow scrollability for the manager layout

--- capstone_pms/src/layouts/ManagerLayout.tsx
+++ capstone_pms/src/layouts/ManagerLayout.tsx
@@ -3,9 +3,9 @@ import Sidebar from "../components/Sidebar"
 
 const ManagerLayout = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen w-full overflow-hidden">
       <Sidebar role="manager" />
-      <main className="flex-1 p-8">
+      <main className="flex-1 min-w-0 overflow-y-auto px-6 py-8 lg:px-10">
         <Outlet />
       </main>
     </div>
